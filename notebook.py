38a9576e7c7c8aa7a1515c54313c21025477224b
Helia1392 <m1358m@gmail.com>
2024-11-29 22:37:40 +0330

    Add files via upload

---
+++ notebook.py
@@ -0,0 +1,16 @@
+from tkinter import*
+from  tkinter import ttk
+window= Tk()
+window.geometry('420x420')
+notebook = ttk.Notebook(window)
+tab1=Frame(notebook)
+tab2=Frame(notebook)
+tab3=Frame(notebook)
+notebook.add(tab1,text='tab 1')
+notebook.add(tab2 , text = 'tab2')
+notebook.add(tab3 , text = 'tab3')
+notebook.pack(expand= TRUE,fill= BOTH)
+Label(tab1, text=' hello this is tab 1', width=50,height=25, bg= '#456377').pack()
+Label(tab2, text= 'hello this is tab 2',width=50,height=25,bg= '#987043').pack()
+Label(tab3, text= 'hello this is tab 3',width=50,height=25,bg='#673451').pack()
+window.mainloop()
